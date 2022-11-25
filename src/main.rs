d630bcf3949f396e27f03f6ae694446ca2456e40
Stéphane Albanese <albanese.stephane@gmail.com>
2022-11-25 23:45:29 +0100

    Some refactoring and new calculations + fade effect on note off

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use midir::MidiInputConnection;
 use nannou::prelude::*;
-use particles::{Particle, Modifier};
+use particles::{RotatingParticle, Modifier};
 use std::{sync::mpsc::{channel, Receiver}, collections::HashMap};
-use wmidi::{ControlFunction, MidiMessage, U7, Note};
+use wmidi::{ControlFunction, MidiMessage, Note};
+
+#[macro_use]
+extern crate dotenv_codegen;
 
 pub mod midi;
 pub mod particles;
@@ -11,13 +14,14 @@ fn main() {
     nannou::app(model).update(update).run();
 }
 
-const NB_PARTICLES: usize = 50;
-const MAX_RADIUS_SCALE: f32 = 10.0;
-const MAX_ACCELERATOR: f32 = 5.0;
+const MAX_RADIUS_SCALE: f32 = 5.;
+const MAX_ACCELERATOR: f32 = 5.;
+const MIDI_MAX_VALUE: u8 = 127;
+const ROTATING_MIN_NOTE: u8 = 48;
+const ROTATION_MAX_NOTE: u8 = 72;
 
 struct Model {
-    particles: Vec<Particle>,
-    notes: HashMap<Note, Point2>,
+    rotating_particles: HashMap<Note, RotatingParticle>,
     modifier: Modifier,
     _connection: Option<MidiInputConnection<()>>,
     receiver: Receiver<Vec<u8>>,
@@ -30,18 +34,11 @@ fn model(app: &App) -> Model {
         .view(view)
         .build()
         .unwrap();
-    let mut particles = Vec::with_capacity(NB_PARTICLES);
-
-    for _i in 0..NB_PARTICLES {
-        let particle = Particle::new(&app.window_rect());
-        particles.push(particle);
-    }
 
     let (tx, rx) = channel();
 
     Model {
-        particles,
-        notes: HashMap::new(),
+        rotating_particles: HashMap::new(),
         modifier: Modifier::new(),
         _connection: midi::init(tx),
         receiver: rx,
@@ -56,26 +53,41 @@ fn update(app: &App, model: &mut Model, _update: Update) {
             }
             Ok(midi_message) => {
                 match midi_message {
+                    // CC control modifiers
                     wmidi::MidiMessage::ControlChange(_, control, value) => {
-                        if control == ControlFunction::EFFECTS_3_DEPTH {
+                        if control == ControlFunction::MODULATION_WHEEL {
                             model.modifier.scale = 1.0 + (MAX_RADIUS_SCALE - 1.0)
                                 * <u8 as From<wmidi::U7>>::from(value) as f32
-                                / <u8 as From<wmidi::U7>>::from(U7::MAX) as f32;
+                                / (MIDI_MAX_VALUE as f32);
                         } else if control == ControlFunction::SOUND_CONTROLLER_5 {
                             model.modifier.accelerator = 1.0 + (MAX_ACCELERATOR - 1.0)
                                 * <u8 as From<wmidi::U7>>::from(value) as f32
-                                / <u8 as From<wmidi::U7>>::from(U7::MAX) as f32;
+                                / (MIDI_MAX_VALUE as f32);
                         }
                     }
-                    wmidi::MidiMessage::NoteOn(_channel, note, _velocity) => {
-                        let width = app.main_window().rect().w();
-                        let note_value = <u8 as From<Note>>::from(note) as f32;
-                        let note_max = <u8 as From<Note>>::from(Note::HIGHEST_NOTE) as f32;
-                        let x = width * note_value / note_max - width / 2.;
-                        model.notes.insert(note, vec2(x, - app.main_window().rect().h() / 2.));
+                    // Notes add new shapes to the scene.
+                    wmidi::MidiMessage::NoteOn(_channel, note, velocity) => {
+                        let w_h = app.main_window().rect().w_h();
+                        let note_value = <u8 as From<Note>>::from(note);
+                        let vel_value = <u8 as From<wmidi::U7>>::from(velocity);
+                        let x: f32;
+                        let y: f32;
+                        match note_value {
+                            ROTATING_MIN_NOTE..=ROTATION_MAX_NOTE => {
+                                let slope = w_h.0 / (ROTATION_MAX_NOTE as f32 - ROTATING_MIN_NOTE as f32);
+                                x = slope * (note_value as f32 - ROTATING_MIN_NOTE as f32) - w_h.0 / 2.;
+                                y = w_h.1 / 2. * (vel_value as f32) / (MIDI_MAX_VALUE as f32) - w_h.1 / 2.;
+                            },
+                            _ => {
+                                x = w_h.0 * (note_value as f32) / (MIDI_MAX_VALUE as f32) - w_h.0 / 2.;
+                                y = - w_h.1 / 2. * (vel_value as f32) / (MIDI_MAX_VALUE as f32) + w_h.1 / 2.;
+                            }
+                        }
+                        
+                        model.rotating_particles.insert(note, RotatingParticle::new(vec2(x, y), vel_value));
                     }
                     wmidi::MidiMessage::NoteOff(_channel, note, _velocity ) => {
-                        model.notes.remove_entry(&note);
+                        model.rotating_particles.get_mut(&note).unwrap().held = false; // Start fading when the note is released.
                     }
                     _ => (),
                 }
@@ -83,13 +95,13 @@ fn update(app: &App, model: &mut Model, _update: Update) {
         };
     }
 
+    // Remove the particles that have expired their lifetime.
+    model.rotating_particles.retain(|_, particle| particle.lifetime > 0);
+
     // Add some variation depending on time
     model.modifier.center_shift = vec2(app.time.cos() * 10.0, app.time.sin() * 10.0);
-    model.particles.iter_mut().for_each(|particle| {
-        particle.update(&model.modifier);
-    });
-    model.notes.iter_mut().for_each(|note| { 
-        note.1.y += 9.;
+    model.rotating_particles.iter_mut().for_each(|particle| {
+        particle.1.update(&model.modifier);
     });
 }
 
@@ -105,16 +117,9 @@ fn view(app: &App, model: &Model, frame: Frame) {
         .w_h(app.main_window().rect().w() as f32, app.main_window().rect().h() as f32)
         .color(srgba(0.0, 0.0, 0.0, 0.05));
 
-    // Draw a line from the last position to the new one + a circle at the end.
-    model.particles.iter().for_each(|particle| {
-        particle.draw(&draw);
-    });
-
-    model.notes.iter().for_each(|note| {
-        draw.ellipse()
-            .x_y(note.1.x, note.1.y)
-            .radius(12.)
-            .color(nannou::color::LIME);
+    // Draw the particles
+    model.rotating_particles.iter().for_each(|particle| {
+        particle.1.draw(&draw);
     });
 
     draw.to_frame(app, &frame).unwrap();
--- src/particles.rs
+++ src/particles.rs
@@ -1,60 +1,62 @@
 use nannou::{prelude::*, rand};
 use rand::Rng;
+use crate::MIDI_MAX_VALUE;
 
 const RADIUS: f32 = 80.0;
 
-pub struct Particle {
+pub struct RotatingParticle {
+    pub held: bool,
     pub position: Point2,
     pub previous: Point2,
     center: Point2,
     pub size: f32,
     angle: f32,
     speed: f32,
-    target_size: f32,
     orbit: f32,
     pub color: Srgb<u8>,
+    pub lifetime: i32,
 }
 
-impl Particle {
-    pub fn new(bounds: &Rect<f32>) -> Self {
+impl RotatingParticle {
+    pub fn new(center: Point2, velocity: u8) -> Self {
         // Generate a random RGB color.
         let mut rand_gen = rand::thread_rng();
         let red: u8 = rand_gen.gen();
         let green: u8 = rand_gen.gen();
         let blue: u8 = rand_gen.gen();
 
-        let half_w = bounds.w() / 2.;
-        let half_h = bounds.h() / 2.;
-        let x = rand_gen.gen_range(-half_w..half_w);
-        let y = rand_gen.gen_range(-half_h..half_h);
-
         // Create a default particle with some random elements.
-        Particle {
-            position: vec2(0., 0.),
-            previous: vec2(0., 0.),
-            center: vec2(x, y),
+        RotatingParticle {
+            held: true,
+            position: center,
+            previous: center,
+            center,
             size: 2.0,
-            angle: 0.0,
+            angle: 0.,
             speed: 0.01 + random_f32() * 0.04,
-            target_size: 5.0,
-            orbit: RADIUS * 0.5 + (RADIUS * 0.5 * random_f32()),
+            orbit: RADIUS * 0.5 + (RADIUS * velocity as f32 / MIDI_MAX_VALUE as f32),
             color: Srgb::new(red, green, blue),
+            lifetime: 100
         }
     }
 
     pub fn update(&mut self, modifier: &Modifier) {
+        if !self.held { self.lifetime -= 1; } // The particle will be removed when it reaches 0.
         self.previous = self.position;
 
         self.angle += self.speed * modifier.accelerator;
 
         self.position.x = (self.center.x + modifier.center_shift.x) + self.angle.cos() * self.orbit * modifier.scale;
         self.position.y = (self.center.y + modifier.center_shift.y) + self.angle.sin() * self.orbit * modifier.scale;
-
-        self.size += (self.target_size - self.size) * 0.2;
-        self.target_size = 1.0 + random_range(0.0, 5.0);
     }
 
     pub fn draw(&self, draw: &Draw) {
+        // Do not draw the first time because position has not been calculated yet.
+        if self.previous == self.center {
+            return;
+        }
+
+        // Draw a line from the last position to the new one + a circle at the end.
         draw.line()
             .points(self.previous, self.position)
             .stroke_weight(self.size)
